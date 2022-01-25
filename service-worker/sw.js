7178be39c13dbe05e690cf7efa3818f95971480a
Jerry Lin <jerry.jzlin@gmail.com>
2022-01-26 00:29:52 +0800

    feat: add fetch event handler

--- service-worker/sw.js
+++ service-worker/sw.js
@@ -15,3 +15,18 @@ self.addEventListener('install', function(event) {
       })
   );
 });
+
+self.addEventListener('fetch', function(event) {
+  console.log('fetch event: ', event);
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
