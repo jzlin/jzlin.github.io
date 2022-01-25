8b67ff30f063d8677309fe57580f90408270beeb
Jerry Lin <jerry.jzlin@gmail.com>
2022-01-26 00:17:55 +0800

    feat: add cache

--- service-worker/sw.js
+++ service-worker/sw.js
@@ -1,1 +1,17 @@
 console.log("running sw.js");
+
+var CACHE_NAME = 'v1';
+var urlsToCache = [
+  '/service-worker/index.html',
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
